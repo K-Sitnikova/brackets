ab29374b2ab20adf2124217436f6c90c4ee13032
Ksenia Sitnikova <xenyasitnikova88@gmail.com>
2024-09-27 13:06:01 +0300

    feat: solve task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,32 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  const stack = []
+  const openBrackets = new Set()
+  const map = {}
+
+  for (let [open, close] of bracketsConfig) {
+    openBrackets.add(open)
+    map[close] = open
+  }
+
+  for (let char of str) {
+    if (openBrackets.has(char)) {// если на входе открывающая скобка
+      if (map[char] === char) {
+        if (stack.length > 0 && stack[stack.length - 1] === char) {
+          stack.pop()
+        } else {
+          stack.push(char)
+        }
+      } else {
+        stack.push(char)
+      }
+    } else {//если на входе закрывающая скобка
+      if (stack.length === 0 || stack[stack.length - 1] !== map[char]) {
+        return false
+      } else {
+        stack.pop()
+      }
+    }
+  }
+
+  return stack.length === 0
 }
